16c39c5f865c151731d8cb437a214d67909bcd10
WEB <WEB@192.168.2.5>
2016-04-10 03:39:57 -0700

    Bootstrap template

--- bootstrap.php
+++ bootstrap.php
@@ -6,4 +6,5 @@
 <link rel="stylesheet" href="<?php echo $_SITE_BASE_URL; ?>/static/bootstrap/css/bootstrap-theme.min.css" >
 
 <!-- Latest compiled and minified JavaScript -->
+<script src="<?php echo $_SITE_BASE_URL; ?>/static/jquery/jquery-2.2.3.min.js" ></script>
 <script src="<?php echo $_SITE_BASE_URL; ?>/static/bootstrap/js/bootstrap.min.js" ></script>
